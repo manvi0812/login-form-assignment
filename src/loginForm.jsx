83b74775211b3c239a8b6c129f15f5796bafcfeb
manvi0812 <59312850+manvi0812@users.noreply.github.com>
2021-04-08 18:45:04 +0530

    Update loginForm.jsx

--- src/loginForm.jsx
+++ src/loginForm.jsx
@@ -3,38 +3,54 @@ import "./loginForm.css";
 import Input from "./shared/formUIElements/Input";
 import Button from "./shared/formUIElements/Button";
 import { useForm } from "./shared/hooks/form-hook";
-import {
-  VALIDATOR_EMAIL,
-  VALIDATOR_PASSWORD,
-} from "./shared/util/validators";
+import { VALIDATOR_EMAIL, VALIDATOR_PASSWORD } from "./shared/util/validators";
 
+const apiEndPoint = "https://minimumque.herokuapp.com/login";
 const LoginForm = () => {
-  const [formState, inputHandler] = useForm({
-    email: {
-      value: "",
-      isValid: false,
+  const [formState, inputHandler] = useForm(
+    {
+      username: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
     },
-    password: {
-      value: "",
-      isValid: false,
-    },
-  });
+    false
+  );
 
-  // const submitHandler = (event) => {
-  //   event.preventDefault();
-  //   console.log(formState.inputs);
-  // };
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const obj = {
+      pwd: formState.inputs.password.value,
+      unme: formState.inputs.username.value,
+    };
+    // const post = await axios.post(apiEndPoint, obj);
+    // const pos = post.data;
+    // console.log(pos);
+    // console.log(formState.inputs.password.value);
+    fetch(apiEndPoint, {
+      method: "POST",
+      body: JSON.stringify(obj),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.log("Error:", error))
+      .then((response) => console.log(obj, response));
+  };
 
   return (
-    <form action='#' >
+    <form onSubmit={submitHandler}>
       <div className='form'>
         <Input
           element='input'
-          id='email'
-          type='email'
+          id='username'
+          type='username'
           label='Username'
           validators={[VALIDATOR_EMAIL()]}
-          errorText='Please enter a valid email address. '
+          errorText='Please enter a valid username. '
           onInput={inputHandler}
           retrieve='Retrieve'
         />
